af122a1d7d6f6aca2543b0fd5385646146cd4511
adcree90 <gpnrd79tjw@privaterelay.appleid.com>
2026-08-17 08:30:52 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "dmp-v4";
+const CACHE = "dmp-v5";
 const ASSETS = ["./", "./index.html", "./manifest.json"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
